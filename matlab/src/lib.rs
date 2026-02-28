ec1fab488f780f54a769f1bb5f022362f95fc67d
Brodi Elwood <bdelwood@fas.harvard.edu>
2026-02-28 14:11:25 -0500

    feat(matlab): Update to Matlab interface.
    Now returns nested struct of all registers like C implementation. Refactored to use extended traits pattern like the Python bindings.

--- matlab/src/lib.rs
+++ matlab/src/lib.rs
@@ -3,13 +3,86 @@ use rustmex::prelude::*;
 use std::path::Path;
 
 use arcfile_rs::arcfile::ArcFile;
-use arcfile_rs::register::TypedRegData;
+use arcfile_rs::register::{RegData, RegValues};
 use rustmex::MatlabClass;
 use rustmex::char::CharArray;
 
+use rustmex::structs::{ScalarStruct, Struct};
+use std::ffi::CStr;
+use std::ffi::CString;
+
 use std::time::Instant;
 
-// TODO: make ToMx trait for type conversion mirrored off Python interface to use in if let Some block
+trait ToMex {
+    fn into_mex(self) -> rustmex::Result<MxArray>;
+}
+
+impl ToMex for RegData {
+    fn into_mex(self) -> rustmex::Result<MxArray> {
+        // basically make_array from Python bindings
+        // with some extra care to handle errors, since we have
+        // to handle these more explicitly with rustmex than with PyO3
+        macro_rules! make_numeric {
+            ($v:expr) => {{
+                let mx = Numeric::new($v.into_boxed_slice(), &[self.nsamp, self.nchan]).map_err(
+                    |_| -> rustmex::Error {
+                        rustmex::message::AdHoc("readarc:alloc", "Failed to create array").into()
+                    },
+                )?;
+
+                Ok(mx.into_inner())
+            }};
+        }
+
+        // row-major...
+        match self.data {
+            RegValues::U8(v) => make_numeric!(v),
+            RegValues::I8(v) => make_numeric!(v),
+            RegValues::U16(v) => make_numeric!(v),
+            RegValues::I16(v) => make_numeric!(v),
+            RegValues::U32(v) => make_numeric!(v),
+            RegValues::I32(v) => make_numeric!(v),
+            RegValues::F32(v) => make_numeric!(v),
+            RegValues::F64(v) => make_numeric!(v),
+            RegValues::Bool(v) => make_numeric!(v),
+
+            // Utc is Vec<[u32; 2]>,
+            // ie pair per row
+            RegValues::Utc(v) => {
+                let n = v.len();
+
+                // matlab column-major:
+                // first all of column 1, then all of column 2
+                let mut flat = Vec::<u32>::with_capacity(n * 2);
+                flat.extend(v.iter().map(|p| p[0]));
+                flat.extend(v.iter().map(|p| p[1]));
+
+                let mx = Numeric::new(flat.into_boxed_slice(), &[n, 2]).map_err(
+                    |_| -> rustmex::Error {
+                        rustmex::message::AdHoc("readarc:alloc", "Failed to create array").into()
+                    },
+                )?;
+
+                Ok(mx.into_inner())
+            }
+        }
+    }
+}
+
+fn to_cstring(s: &str) -> rustmex::Result<CString> {
+    CString::new(s)
+        .map_err(|_| rustmex::message::AdHoc("readarc:cstring", "Name contains null byte").into())
+}
+
+fn make_scalar_struct<'a, K: Iterator<Item = &'a String>>(
+    keys: K,
+) -> rustmex::Result<ScalarStruct<MxArray>> {
+    let cstring: Vec<CString> = keys
+        .map(|k| to_cstring(&k))
+        .collect::<Result<Vec<_>, _>>()?;
+    let refs: Vec<&CStr> = cstring.iter().map(|s| s.as_c_str()).collect();
+    Ok(Struct::new(&[1, 1], &refs).into_scalar().unwrap())
+}
 
 #[rustmex::entrypoint]
 fn readarc_rs(lhs: Lhs, rhs: Rhs) -> rustmex::Result<()> {
@@ -26,15 +99,6 @@ fn readarc_rs(lhs: Lhs, rhs: Rhs) -> rustmex::Result<()> {
     // Convert char to cstring to String
     let filename: String = filename_char.get_cstring().to_string_lossy().into_owned();
 
-    // Do same raw mx array > char > cstring > String for register name
-    let regname_mx = rhs
-        .get(1)
-        .error_if_missing("readarc:no_reg", "Missing register name")?;
-    let regname_char = CharArray::from_mx_array(regname_mx).map_err(|_| {
-        rustmex::message::AdHoc("readarc:bad_type", "Register name must be a char array")
-    })?;
-    let regname: String = regname_char.get_cstring().to_string_lossy().into_owned();
-
     // Open and parse
     let t0 = Instant::now();
 
@@ -47,63 +111,38 @@ fn readarc_rs(lhs: Lhs, rhs: Rhs) -> rustmex::Result<()> {
     let t1 = Instant::now();
 
     // Like Python bindings:
-    // avoid copying, prefer to have mex  take ownership
-    let reg = af.get_mut(&regname).ok_or_else(|| {
-        rustmex::message::AdHoc("readarc:noreg", format!("Register not found: {}", regname))
-    })?;
+    // avoid copying, prefer to have mex take ownership
+    let regtree = af.into_tree();
 
-    let data = reg.data.take().ok_or_else(|| {
-        rustmex::message::AdHoc("readarc:excluded", format!("{} is not archived.", regname))
-    })?;
+    // make top level stuct
+    let mut map_struct = make_scalar_struct(regtree.keys())?;
+    // loop over maps
+    for (map_name, boards) in regtree {
+        let mut board_struct = make_scalar_struct(boards.keys())?;
+        // loop over boards
+        for (board_name, blocks) in boards {
+            let mut block_struct = make_scalar_struct(blocks.keys())?;
+            // loop over blocks
+            for (block_name, block) in blocks {
+                // actually extract and convert data type
+                let mx = block.into_mex()?;
+                // fill in block struct
+                block_struct.set(to_cstring(&block_name)?.as_c_str(), mx)?;
+            }
+            // fill in board struct
+            board_struct.set(
+                to_cstring(&board_name)?.as_c_str(),
+                block_struct.into_inner(),
+            )?;
+        }
+        // fill in map strict
+        map_struct.set(to_cstring(&map_name)?.as_c_str(), board_struct.into_inner())?;
+    }
 
     // Write to first output
     // if let Some because user may not have asked for first slot in assignment
     if let Some(ret) = lhs.get_mut(0) {
-        // basically make_array from Python bindsings
-        // with some extra care to handle errors, since we have
-        // to handle these more explicitly with rustmex than with PyO3
-        macro_rules! write_numeric {
-            ($v:expr) => {{
-                let n = $v.len();
-                let mx = Numeric::new($v.into_boxed_slice(), &[n, 1]).map_err(
-                    |_| -> rustmex::Error {
-                        rustmex::message::AdHoc("readarc:alloc", "Failed to create array").into()
-                    },
-                )?;
-                ret.replace(mx.into_inner());
-            }};
-        }
-
-        // TODO: implement rest of types
-        // row-major...
-        match data {
-            TypedRegData::U32(v) => write_numeric!(v),
-            TypedRegData::I32(v) => write_numeric!(v),
-            TypedRegData::F32(v) => write_numeric!(v),
-            TypedRegData::F64(v) => write_numeric!(v),
-
-            // Assuming Utc is something like Vec<[u32; 2]>,
-            // ie pair per row
-            TypedRegData::Utc(v) => {
-                let n = v.len();
-
-                // MATLAB column-major:
-                // first all of column 1, then all of column 2
-                let mut flat = Vec::<u32>::with_capacity(n * 2);
-                flat.extend(v.iter().map(|p| p[0]));
-                flat.extend(v.iter().map(|p| p[1]));
-
-                let mx = Numeric::new(flat.into_boxed_slice(), &[n, 2]).map_err(
-                    |_| -> rustmex::Error {
-                        rustmex::message::AdHoc("readarc:alloc", "Failed to create array").into()
-                    },
-                )?;
-
-                ret.replace(mx.into_inner());
-            }
-
-            _ => rustmex::error!("readarc:type", "Unsupported register type"),
-        }
+        ret.replace(map_struct.into_inner());
     }
 
     eprintln!("Convert: {:?}", t1.elapsed());
